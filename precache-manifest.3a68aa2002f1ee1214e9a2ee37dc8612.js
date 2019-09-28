daa1611d93eef7e427595664342f8e5ae0946086
yerofey <hi@yerofey.ru>
2019-09-28 23:15:54 +0300

    init

---
+++ precache-manifest.3a68aa2002f1ee1214e9a2ee37dc8612.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "40e09b6a0c9a29c6b500f22e13ba0715",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a3972a78d8844aef6bfb",
+    "url": "/static/css/2.4e89ce74.chunk.css"
+  },
+  {
+    "revision": "c23fd38612a82c5fc5b8",
+    "url": "/static/css/main.88141ff4.chunk.css"
+  },
+  {
+    "revision": "a3972a78d8844aef6bfb",
+    "url": "/static/js/2.bbecfec0.chunk.js"
+  },
+  {
+    "revision": "c23fd38612a82c5fc5b8",
+    "url": "/static/js/main.0ab5848b.chunk.js"
+  },
+  {
+    "revision": "608c3a842b04dc0fd748",
+    "url": "/static/js/runtime-main.7ed0fa20.js"
+  }
+]);
